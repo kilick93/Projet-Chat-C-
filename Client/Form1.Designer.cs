62990b07f07cf646a5178fbe580e6c4f3fb1ba07
Aweuzegaga <mathieu.yahiko@hotmail.fr>
2015-03-16 11:11:35 +0100

    Connexion du client au serveur

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -28,21 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.text_IP = new System.Windows.Forms.TextBox();
+            this.text_Port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_connect = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // text_IP
+            // 
+            this.text_IP.Location = new System.Drawing.Point(85, 39);
+            this.text_IP.Name = "text_IP";
+            this.text_IP.Size = new System.Drawing.Size(100, 20);
+            this.text_IP.TabIndex = 1;
+            // 
+            // text_Port
+            // 
+            this.text_Port.Location = new System.Drawing.Point(85, 79);
+            this.text_Port.Name = "text_Port";
+            this.text_Port.Size = new System.Drawing.Size(100, 20);
+            this.text_Port.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(98, 119);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 5;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_Port);
+            this.Controls.Add(this.text_IP);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox text_IP;
+        private System.Windows.Forms.TextBox text_Port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_connect;
     }
 }
 
